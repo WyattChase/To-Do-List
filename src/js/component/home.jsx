781c72a600cee8bbab79f80e53efa637d11acee6
WyattChase <esnaruto@yahoo.com>
2023-03-04 02:23:43 +0000

    lets begin

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+//create your first component 
 const Home = () => {
+	const [inputValue, setInputValue ] = useState()
+	const [todos, setTodos] = useState()
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+
+			<h1>ToDos</h1>
+			<ul>
+				<li><input type="text" 
+						onChange={(e) => setInputValue.target.value }
+						value={inputValue}
+						onKeyDown={(e) => e.charCode === 13 ? setTodos(setTodos.concat(inputValue)) : null}
+						placeholder="What do you need to do"></input></li>
+				<li>Make the bed</li>
+				<li>Pay the bills</li>
+				<li>Brush Teeth</li>
+				<li>Code a program</li>
+			</ul>
+			<div type="footer">5 Left</div>
+	
 		</div>
 	);
 };
